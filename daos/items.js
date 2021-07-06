d4abda8b31cc226ee02f8b8036cd8a2672c10ab5
Kateryna Masiuk <kmasiuk@uw.edu>
2021-07-05 20:27:25 -0700

    week-1 finished

--- daos/items.js
+++ daos/items.js
@@ -6,23 +6,31 @@ module.exports.items = [];
 
 module.exports.getAll = () => {
   return module.exports.items;
-}
+};
 
 module.exports.getById = (itemId) => {
-  // TODO: complete
-}
+  if(itemId) {  
+    return module.exports.items.find(item => item.id === itemId);
+  } else {
+    return null;
+  }
+};
 
 module.exports.deleteById = async (itemId) => {
-    // TODO: complete
-}
+  const removeId = module.exports.items.findIndex(item => item.id === itemId);
+  return module.exports.items.splice(removeId, 1);
+};
 
 module.exports.updateById = async (itemId, newObj) => {
-    // TODO: complete
-}
+  const updateId = module.exports.items.findIndex(item => item.id === itemId);
+  if(updateId >= 0) {
+    return module.exports.items[updateId].field = newObj.field;
+  }
+};
 
 module.exports.create = async (item) => {
   const id = uuid.v4();
   const newItem = { ...item, id };
   module.exports.items.push(newItem);
   return newItem;
-}
+};
