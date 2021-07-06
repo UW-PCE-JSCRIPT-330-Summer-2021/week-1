4baa2877fc5167976a6085432072e57e36e27120
Julio <jcpayano@hotmail.com>
2021-07-06 12:29:17 -0400

    week-1 assignement

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
+const morgan = require('morgan');
 
-router.use("/items", require("./items"));
+router.use(morgan('dev'));
+
+router.use('/items', require('./items'));
+
+router.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status(404);
+  next(error);
+});
+
+router.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = router;
--- routes/items.js
+++ routes/items.js
@@ -1,30 +1,39 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const router = Router();
 
 const itemDao = require('../daos/items');
 
-router.get("/", (req, res, next) => {
-  res.json(itemDao.getAll())
+router.get('/', (req, res, next) => {
+  res.json(itemDao.getAll());
 });
 
-router.get("/:id", (req, res, next) => {
+router.get('/:id', (req, res, next) => {
   // TODO: complete this route
-  res.sendStatus(501);
+  const { id } = req.params;
+  res.json(itemDao.getById(id));
+  res.sendStatus(200);
 });
 
-router.post("/", (req, res, next) => {
+router.post('/', (req, res, next) => {
   itemDao.create(req.body);
   res.sendStatus(200);
 });
 
-router.put("/:id", (req, res, next) => {
+router.put('/:id', (req, res, next) => {
   // TODO: complete this route
-  res.sendStatus(501);
+  const { id } = req.params;
+
+  res.json(itemDao.updateById(id, req.body));
+  
+  // res.sendStatus(200);
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
   // TODO: complete this route
-  res.sendStatus(501);
+  const { id } = req.params;
+  res.json(itemDao.deleteById(id));
+  res.send(`item  frute deteted ${id}`);
+  res.sendStatus(200);
 });
 
 module.exports = router;
