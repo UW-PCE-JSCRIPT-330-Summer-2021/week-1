8769450cf6128821859a40d835bf065b746e3bb1
Maboh Christopher <christopher.maboh@yahoo.com.com>
2021-07-06 20:07:56 -0400

    week1-assignment

--- daos/items.js
+++ daos/items.js
@@ -10,14 +10,25 @@ module.exports.getAll = () => {
 
 module.exports.getById = (itemId) => {
   // TODO: complete
+  let index =module.exports.items.find(item => item.id === itemId)
+  if (index) { return index} else {return null}
 }
 
 module.exports.deleteById = async (itemId) => {
     // TODO: complete
+    let removeIndex =module.exports.items.map(item =>item.id). indexOf(itemID)
+    if (removeIndex >= 0){
+      module.exports.items.splice(removeIndex, 1)
+    }
+      
 }
 
 module.exports.updateById = async (itemId, newObj) => {
     // TODO: complete
+    let index = module.exports.items.find(item => item.id ===itemId)
+    if(index) {
+      matchMedia.field = newObj.field
+    }
 }
 
 module.exports.create = async (item) => {
