03bc5416207166547746481b76b1180336b1b1c0
annmarieavendano <82134173+annmarieavendano@users.noreply.github.com>
2021-07-08 16:50:55 -0700

    Update items.js

--- routes/items.js
+++ routes/items.js
@@ -3,28 +3,43 @@ const router = Router();
 
 const itemDao = require('../daos/items');
 
-router.get("/", (req, res, next) => {
+router.get("/", (req, res, next) =>
+{
   res.json(itemDao.getAll())
 });
 
-router.get("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+router.get("/:id", (req, res, next) => 
+{
+  let temp = itemDao.getById(req.params.id);
+
+  if (temp !== undefined)
+  {
+    res.json(temp);
+    res.sendStatus(200);
+  }
+  else
+  {
+    res.sendStatus(404);
+  }
 });
 
-router.post("/", (req, res, next) => {
+router.post("/", (req, res, next) =>
+{
   itemDao.create(req.body);
   res.sendStatus(200);
 });
 
-router.put("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+router.put("/:id", (req, res, next) => 
+{
+  // TODO: fix this function
+  itemDao.updateById(req.params.id);
+  res.sendStatus(200);
 });
 
-router.delete("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+router.delete("/:id", (req, res, next) => 
+{
+  itemDao.deleteById(req.params.id);
+  res.sendStatus(200);
 });
 
 module.exports = router;
