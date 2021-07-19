94852a6593b747f4c414db66cb34a2b08bc8d245
JS-Q <76979814+JS-Q@users.noreply.github.com>
2021-07-19 00:47:12 -0500

    HW1Upload

--- routes/items.js
+++ routes/items.js
@@ -7,9 +7,14 @@ router.get("/", (req, res, next) => {
   res.json(itemDao.getAll())
 });
 
-router.get("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+router.get('/:id', (req, res, next) => {
+	const item = itemDao.getById(req.params.id);
+	if (item) {
+		res.json(item);
+		res.sendStatus(200);
+	} else {
+		res.sendStatus(404);
+	}
 });
 
 router.post("/", (req, res, next) => {
@@ -17,14 +22,14 @@ router.post("/", (req, res, next) => {
   res.sendStatus(200);
 });
 
-router.put("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+router.put('/:id', (req, res, next) => {
+	res.json(itemDao.updateById(req.params.id, req.body));
+	res.sendStatus(200);
 });
 
-router.delete("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+router.delete('/:id', (req, res, next) => {
+	res.json(itemDao.deleteById(req.params.id));
+	res.sendStatus(200);
 });
 
 module.exports = router;
