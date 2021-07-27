a833f8360bc984f8f7a52700425a70091e6c6d22
Morgan Corbridge <pcpower1@gmail.com>
2021-07-26 18:36:12 -0700

    Week 1 complete

--- daos/items.js
+++ daos/items.js
@@ -9,15 +9,32 @@ module.exports.getAll = () => {
 }
 
 module.exports.getById = (itemId) => {
-  // TODO: complete
+  const item = module.exports.items.find( c=> c.id === itemId);
+  if (item == null) {
+    return null;
+  }
+  return {...item};
 }
 
 module.exports.deleteById = async (itemId) => {
-    // TODO: complete
+  const itemNumber = module.exports.items.findIndex(i => i.id === itemId);
+  module.exports.items.splice(itemNumber, 1);
+  
 }
 
 module.exports.updateById = async (itemId, newObj) => {
-    // TODO: complete
+  const existingItem = module.exports.getById(itemId);
+  if (existingItem == null) {
+    return -1; 
+  } else {
+
+    const newItem = { ...newObj};
+    console.log(`ID: ${newObj.id}`);
+    console.log(`Field: ${newObj.field}`);
+    existingItem.field = newItem.field;
+    const itemNumber = module.exports.items.findIndex(i => i.id === itemId);
+    module.exports.items.splice(itemNumber, 1, existingItem);
+  }
 }
 
 module.exports.create = async (item) => {
--- routes/items.js
+++ routes/items.js
@@ -4,27 +4,41 @@ const router = Router();
 const itemDao = require('../daos/items');
 
 router.get("/", (req, res, next) => {
+
   res.json(itemDao.getAll())
 });
 
 router.get("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+  
+const itemId = itemDao.getById(req.params.id);
+  if (itemId == null) {
+    return res.status(404).send("Error: Items not available")
+  }
+  res.json(itemId);
+  next;
 });
 
 router.post("/", (req, res, next) => {
   itemDao.create(req.body);
   res.sendStatus(200);
+  next;
 });
 
 router.put("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+  const requestBody = {...req.body};
+  const itemId = itemDao.getById(req.params.id);
+  if (itemId != null) {
+    
+    itemDao.updateById(req.params.id, requestBody);
+  }
+  res.sendStatus(200);
+  next;
 });
 
 router.delete("/:id", (req, res, next) => {
-  // TODO: complete this route
-  res.sendStatus(501);
+  itemDao.deleteById(req.params.id);
+  res.sendStatus(200);
+  next;
 });
 
 module.exports = router;
